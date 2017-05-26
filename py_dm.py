da1966bd9a4497367c145cd3febb96320ed9d44a
SEONIL <seonilson92@gmail.com>
2017-05-26 00:10:54 -0400

    need to install hangul bus for ubuntu arghhhhh

--- py_dm.py
+++ py_dm.py
@@ -3,13 +3,37 @@
 import subprocess as sb
 import json as j
 
+#def parseJson(rawout):
+#    j.dump
+#    return Json
+
+def parse_attribute(target, user, cpu, pname, tstmp): #each dtypes are str
+	lines_list=target.splitlines()
+	
+
 def invokeTop():
-    topout=sb.check_output("top").decode("utf-8")
-    return topJson
+    def top_parse(target):
+		lines_list=target.splitlines()#list of lines w/o trailing \n	
+		dump_list=[]
+		with open(topJson.json, "w") as tj:
+			for i in range(8,len(lines_list)):                 
+				split=lines[i].split()
+				obj={}
+				obj["user"]=split[1]			#user=col 1
+				obj["cpu"]=split[8]				#cpu =col 8
+				obj["process_name"]=split[11]	#pname=col 11
+				obj["timestamp"]=split[10]		#tstmp=col 10
+				dump_list.append(obj)
+			tj.dump(dump_list)
+		return None
+
+    topout=sb.check_output("top -o %CPU -n 1 -b > topout", shell=True)
+    top_parse(topout)
+    return None
 
 def invokeWho():
-    sb.run("who")
-    #parse into json
+    whoout=sb.check_output("who")
+    whoJson=parseJson(whoout)
     return whoJson
 
 def mergeJson(whoJson, topJson):
--- test_check.py
+++ test_check.py
@@ -1,4 +1,7 @@
 #test subprocess check_output
 import subprocess as sb 
-a=sb.check_output("ls")
-print(a.decode("utf-8"))
+a=sb.check_output("top -n 1", shell=True).decode("utf-8")
+with open("out.txt","w") as f:
+	f.write(a)
+print("open out.txt")
+sb.run("cat out.txt", shell=True)
